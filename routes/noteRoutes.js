44ab06e08776e2805c2a1ad23b61603641146238
Dani <danisweetwood@gmail.com>
2024-02-23 15:09:21 -0600

    updating

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+app.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
+app.post('/', (req,res) => {
+    console.log(req.body);
+    // let addNote = req.body;
+    // readAndAppend(addNote, './db/db.json')
+});
 
 module.exports = app;
